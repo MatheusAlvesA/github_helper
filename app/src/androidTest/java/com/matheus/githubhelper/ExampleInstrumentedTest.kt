e0e770c4ce3ae9861678b35155a14ec10bde1020
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-10-27 18:09:51 -0300

    Removendo teste default

--- app/src/androidTest/java/com/matheus/githubhelper/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/matheus/githubhelper/ExampleInstrumentedTest.kt
@@ -1,5 +1,5 @@
 package com.matheus.githubhelper
-
+/*
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
 
@@ -22,3 +22,4 @@ class ExampleInstrumentedTest {
         assertEquals("com.matheus.githubhelper", appContext.packageName)
     }
 }
+*/
