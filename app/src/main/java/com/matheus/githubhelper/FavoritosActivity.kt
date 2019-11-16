eea1b27a5f1d7e8b1582df5a91dcf4157b7c0f53
Matheus Alves de Andrade <matheuseejam@gmail.com>
2019-11-16 15:41:03 -0300

    Iniciando activity de favoritos

---
+++ app/src/main/java/com/matheus/githubhelper/FavoritosActivity.kt
@@ -0,0 +1,12 @@
+package com.matheus.githubhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoritosActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favoritos)
+    }
+}
