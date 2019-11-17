60bc19748a6be851ddfb7b21018c5bf335579208
ester <esterdcveiga@gmail.com>
2019-11-17 11:58:24 -0300

    listagem dos commits

---
+++ app/src/main/java/com/matheus/githubhelper/adapter/RepositoryAdapter.kt
@@ -0,0 +1,38 @@
+package com.matheus.githubhelper.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.matheus.githubhelper.R
+import com.matheus.githubhelper.models.Commit
+
+class RepositoryAdapter(
+    private val commits: List<Commit>
+): RecyclerView.Adapter<RepositoryAdapter.VH>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.commit_item, parent, false)
+
+        return VH(v)
+    }
+
+    override fun getItemCount(): Int = commits.size
+
+    override fun onBindViewHolder(holder: VH, position: Int) {
+        val commit = commits.get(position)
+        holder.id.text = commit.id
+        holder.authorName.text = commit.name_author
+        holder.authorEmail.text = commit.email_author
+        holder.message.text = commit.message
+    }
+
+
+    class VH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var id: TextView = itemView.findViewById(R.id.txtId)
+        var authorName: TextView = itemView.findViewById(R.id.txtNameAuthor)
+        var authorEmail: TextView = itemView.findViewById(R.id.txtEmailAuthor)
+        var message: TextView = itemView.findViewById(R.id.txtMessage)
+    }
+}
